b1c6d687adcd787669485d79d9621d941cdc0f8b
Park, Pablo Chanwoo <kookoowaa@gmail.com>
2021-12-16 22:45:55 +0900

    Kotlin Basics

---
+++ KotlinBasics/app/src/main/java/com/example/kotlinbasics/Basics.kt
@@ -0,0 +1,14 @@
+package com.example.kotlinbasics
+
+fun main(){
+
+    // immutable variable
+    // TODO: Add new functionality
+
+    val myName = "Pablo"
+    print("this is ${myName}")
+
+    val myF = 2.22;
+    val myFloat: Float = 13.37F
+}
+
