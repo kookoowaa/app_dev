f52610598705359466faa1d20d7f9327eefef0ab
Park, Pablo Chanwoo <kookoowaa@gmail.com>
2022-01-14 22:26:25 +0900

    updates

--- KotlinBasics/app/src/main/java/com/example/kotlinbasics/Basics.kt
+++ KotlinBasics/app/src/main/java/com/example/kotlinbasics/Basics.kt
@@ -1,14 +1,36 @@
 package com.example.kotlinbasics
 
 fun main(){
+    var testClass = myClass(param1 = "ABC", param2 ="DEF")
 
-    // immutable variable
-    // TODO: Add new functionality
-
-    val myName = "Pablo"
-    print("this is ${myName}")
-
-    val myF = 2.22;
-    val myFloat: Float = 13.37F
+    println(testClass.arg1)
+    // ABC
+    testClass.whatAreArgs()
+    // there are argements here: ABC & DEF
 }
 
+
+class myClass(param1: String, param2: String){
+    // property
+    var arg1: String = param1
+    var arg2: String = param2
+
+    // method
+    fun whatAreArgs(){
+        println("there are argements here: $arg1 & $arg2")
+    }
+}
+
+class MobilePhone(osName: String, brand: String, model: String){
+
+    init {
+        println("The phone $model from $brand uses $osName as its Operating System")
+    }
+
+    var battery : Int = 30
+    fun chargeBattery("Charge")
+
+
+
+
+}
