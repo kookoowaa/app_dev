73fb4f92b46bc59c27b5f7361d5e1453d7d7d9dd
Park, Pablo Chanwoo <kookoowaa@gmail.com>
2022-05-02 19:45:35 +0900

    Operator

--- MyCalc/app/src/main/java/com/example/mycalc/MainActivity.kt
+++ MyCalc/app/src/main/java/com/example/mycalc/MainActivity.kt
@@ -2,6 +2,7 @@ package com.example.mycalc
 
 import androidx.appcompat.app.AppCompatActivity
 import android.os.Bundle
+import android.text.BoringLayout
 import android.view.View
 import android.widget.Button
 import android.widget.TextView
@@ -10,6 +11,8 @@ import android.widget.Toast
 class MainActivity : AppCompatActivity() {
 
     private var tvInput:TextView? = null
+    var lastNumeric : Boolean = false
+    var lastDot: Boolean = false
 
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
@@ -22,9 +25,41 @@ class MainActivity : AppCompatActivity() {
 
     fun onDigit(view: View){
         tvInput?.append((view as Button).text)
+        lastNumeric = true
+        lastDot = false
+
     }
 
     fun onClear(view: View){
         tvInput?.text = ""
+    }
+
+    fun onDecimalPoint(view: View){
+        if(lastNumeric && !lastDot){
+            tvInput?.append("@string/dot")
+            lastNumeric = false
+            lastDot = true
+        }
+    }
+
+    fun onOperator(view: View){
+        tvInput?.text?.let{
+            if (lastNumeric && !isOperatorAdded(it.toString())){
+                tvInput?.append((view as Button).text)
+                lastNumeric = false
+                lastDot = false
+            }
+        }
+    }
+
+    private fun isOperatorAdded(value :String): Boolean{
+        return if(value.startsWith("-")){
+            false
+        }else{
+            value.contains("/") ||
+                    value.contains("+") ||
+                    value.contains("*") ||
+                    value.contains("-")
+        }
     }
 }
