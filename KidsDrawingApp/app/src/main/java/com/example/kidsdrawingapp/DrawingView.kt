59e90bd3a5e21db890b65b1df793d6881c9054b1
Park, Pablo Chanwoo <kookoowaa@gmail.com>
2022-06-08 22:51:00 +0900

    Update DrawingView

--- KidsDrawingApp/app/src/main/java/com/example/kidsdrawingapp/DrawingView.kt
+++ KidsDrawingApp/app/src/main/java/com/example/kidsdrawingapp/DrawingView.kt
@@ -3,6 +3,7 @@ package com.example.kidsdrawingapp
 import android.content.Context
 import android.graphics.*
 import android.util.AttributeSet
+import android.view.MotionEvent
 import android.view.View
 
 
@@ -32,6 +33,56 @@ class DrawingView(context: Context, attrs: AttributeSet): View(context, attrs) {
 
         mCanvasPaint = Paint(Paint.DITHER_FLAG)
         mBrushSize = 20.toFloat()
+    }
+
+    override fun onSizeChanged(w: Int, h: Int, oldw: Int, oldh: Int) {
+        super.onSizeChanged(w, h, oldw, oldh)
+        mCanvasBitmap = Bitmap.createBitmap(w, h, Bitmap.Config.ARGB_8888)
+        canvas = Canvas(mCanvasBitmap!!)
+    }
+
+    // Change Canvas to Canvas? if fails
+    override fun onDraw(canvas: Canvas) {
+        super.onDraw(canvas)
+        canvas.drawBitmap(mCanvasBitmap!!, 0f, 0f, mCanvasPaint)
+
+        if (!mDrawPath!!.isEmpty){
+            mDrawPaint!!.strokeWidth = mDrawPath!!.brushThickness
+            mDrawPaint!!.color = mDrawPath!!.color
+            canvas.drawPath(mDrawPath!!, mDrawPaint!!)
+        }
+
+    }
+
+    override fun onTouchEvent(event: MotionEvent?): Boolean {
+        val touchX = event?.x
+        val touchY = event?.y
+
+        when (event?.action) {
+
+            MotionEvent.ACTION_DOWN -> {
+                mDrawPath!!.color = color
+                mDrawPath!!.brushThickness = mBrushSize
+
+                mDrawPath!!.reset()
+                mDrawPath!!.moveTo(touchX!!, touchY!!)
+            }
+
+            MotionEvent.ACTION_MOVE -> {
+                mDrawPath!!.lineTo(touchX!!, touchY!!)
+            }
+
+            MotionEvent.ACTION_UP -> {
+                mDrawPath = CustomPath(color, mBrushSize)
+
+            }
+
+            else -> {return false}
+
+        }
+
+        invalidate()
+        return true
     }
 
     internal inner class CustomPath(var color: Int,
